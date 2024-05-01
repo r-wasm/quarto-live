4b69b568c6a0ab0397f739c7bb10e6a164432485
George Stagg <george.stagg@posit.co>
2024-05-01 10:06:19 +0100

    Add --watch option to npm build script

---
+++ resources/webr-ojs-runtime/build.ts
@@ -0,0 +1,43 @@
+import { build, context, BuildOptions, PluginBuild } from 'esbuild';
+import process from 'process';
+import { promises as fs } from 'fs';
+
+let watch = false;
+if (process.argv.includes("--watch")) {
+  watch = true;
+}
+
+const cssFiles = [
+  "codemirror-themes-html.css",
+];
+
+const copyPlugin = () => ({
+  name: 'copy-plugin',
+  setup(build: PluginBuild) {
+    build.onLoad({ filter: /.+/ }, () => {
+      return { watchFiles: cssFiles.map((f) => `./src/${f}`) };
+    });
+    build.onEnd(async () => {
+      await Promise.all(cssFiles.map((f) =>
+        fs.copyFile(`./src/${f}`, `./dist/${f}`)
+      ));
+    });
+  },
+});
+
+const options: BuildOptions = {
+  entryPoints: ['./src/main.ts'],
+  bundle: true,
+  outfile: './dist/webr-ojs-runtime.js',
+  minify: true,
+  platform: 'neutral',
+  plugins: [ copyPlugin() ],
+  logLevel: 'info',
+};
+
+if (watch) {
+  const ctx = await context(options);
+  await ctx.watch();
+} else {
+  await build(options);
+}
