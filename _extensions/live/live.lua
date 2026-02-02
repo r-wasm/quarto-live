f06a38b425ca9eaa87586c5e5b82bdfb721bb1f6
George Stagg <george.stagg@posit.co>
2026-02-02 09:41:20 +0000

    Update webR to v0.5.8

--- _extensions/live/live.lua
+++ _extensions/live/live.lua
@@ -596,7 +596,7 @@ function setupWebR(doc)
 
   -- Initial webR startup options
   local webr_options = {
-    baseUrl = "https://webr.r-wasm.org/v0.5.4/",
+    baseUrl = "https://webr.r-wasm.org/v0.5.8/",
   }
   if (webr["engine-url"]) then
     webr_options["baseUrl"] = pandoc.utils.stringify(webr["engine-url"])
